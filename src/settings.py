eb140f36c6421eb31ea55f3e65acd612be28c73f
IainMcl <34077975+Darthmazgar@users.noreply.github.com>
2021-02-26 11:08:09 +0000

    Add default env vars.

--- src/settings.py
+++ src/settings.py
@@ -4,11 +4,11 @@ from decouple import config
 ROOT_DIR: str = os.path.dirname(os.path.abspath(__file__))
 CONFIG_PATH: str = os.path.join(ROOT_DIR, 'configuration.conf')
 
-DB_PATH: str = os.path.join(ROOT_DIR, config("DB"))
+DB_PATH: str = os.path.join(ROOT_DIR, config("DB", default="boatcomp.db"))
 
 # Environment variables
 DEBUG: bool = True
-DB: str = config("DB")
+DB: str = config("DB", default="boatcomp.db")
 
 
 def check_setup():
